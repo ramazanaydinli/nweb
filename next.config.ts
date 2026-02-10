b4ff5f02bf2314385fa69d0f97bd79c3b61e8872
ramazanaydinli <ramazanaydinli94@gmail.com>
2026-02-10 20:15:24 +0300

    settings for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. Statik HTML çıktısı almak için bu satır şart:
+  output: 'export',
+
+  // 2. GitHub Pages'de resim optimizasyonu sunucusu olmadığı için bunu kapatmalıyız:
+  images: {
+    unoptimized: true,
+  },
+  
+  // Not: Özel domain (odtuluakademi.com.tr) kullanacağın için basePath ayarına gerek yok!
 };
 
 export default nextConfig;
